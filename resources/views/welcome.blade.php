8c8a932cf43d0bc912ad6c907d0051c2dce6bbc3
Doni <doniparhan6687@gmail.com>
2018-12-18 14:35:49 +0700

    step 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    IKAME ISTANACARWASH
+                    IKAME ISTANACARWASH 1
                 </div>
 
                 <div class="links">
