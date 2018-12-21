966a0952717ac302d0c5937f123858cd681789e5
aam <am.mj03@gmmail.coom>
2018-12-21 11:13:31 +0700

    tampilan header dasar

--- resources/views/frontend/template/app.blade.php
+++ resources/views/frontend/template/app.blade.php
@@ -56,6 +56,10 @@
         padding-top: 60px;
       }
 
+      .logo-50{
+        width: 60%;
+      }
+
       .header__search__input {
           background: #fff;
           opacity: .9;
@@ -72,10 +76,11 @@
   </head>
 
 <body>
-  <div class="header-top">
+  <div class="header-top bg-orange">
     <div class="container nopadding3">
-      <div class="nav-scroller bg-white hide-on-mobile">
-        <nav class="nav nav-underline">
+      <div class="nav-scroller hide-on-mobile">
+        <nav class="nav nav-underline" style="font-size: 10pt;">
+          <a class="nav-link" href="{{ url('/')}}">Home</a>
           <a class="nav-link" href="#">Cara Belanja</a>
           <a class="nav-link" href="#">Konfirmasi Pembayaran</a>
           <a class="nav-link" href="#">Nomor Rekening </a>
@@ -87,40 +92,37 @@
 
 
   <div id="navbar">
-    
-  <div class="bg-dark">
-  <div class="d-flex flex-column flex-md-row align-items-center px-md-4 bg-white border-bawah box-shadow" id="myHeader">
-  <div class="container mt-2 mb-2">
-    <div class="row">
-      
-          <div class="col-8 col-md-2">
-            <!--<img class="mt-2 mb-2" src="https://barokah.shope/assets/img/barokahshope.png" alt="" width="48" height="48">-->
-           
-            <img id="logoIkm" width="100%" class="my-0 mr-md-auto logo-mb" src="https://i1.wp.com/alatcucianmobiljakarta.com/wp-content/uploads/2018/07/logo_kecil_aplikasi.png" src-orig="https://i1.wp.com/alatcucianmobiljakarta.com/wp-content/uploads/2018/07/logo_kecil_aplikasi.png?fit=300%2C75&amp;ssl=1" scale="1">           
-          </div>
+  <div class="d-flex flex-column flex-md-row align-items-center px-md-4 bg-orange box-shadow" id="myHeader">
+    <div class="container mt-2 mb-2">
+      <div class="row">
+        
+            <div class="col-6 col-md-3">
+              <!--<img class="mt-2 mb-2" src="https://barokah.shope/assets/img/barokahshope.png" alt="" width="48" height="48">-->
+             
+              <img id="logoIkm" width="100%" class="my-0 mr-md-auto logo-mb" src="{{ asset('image/logo_ikame_putih2.png')}}">           
+            </div>
 
-          <div class="col-4 col-md-8 hide-on-mobile">
-            <form method="get" id="searchform" action="https://alatcucianmobiljakarta.com/?cat=37&amp;">
-              <div class="input-group mt-1 ">
-                <input type="search" style="background-color:#f3f3f3; border: 0px solid #ced4da;" class="form-control" placeholder="cari alat cuci mobil dan motor" value="" name="s" id="s">
-              </div>
-            </form>
-          </div>
+            <div class="col-4 col-md-6 hide-on-mobile">
+              <form method="get" id="searchform" action="#">
+                <div class="input-group mt-1 ">
+                  <input type="search" style="background-color:#f3f3f3bf; border: 0px solid #ced4da; border-radius: 0rem" class="form-control" placeholder="cari alat cuci mobil dan motor" value="" name="s" id="s">
+                </div>
+              </form>
+            </div>
 
-          <div class="col-4 col-md-2">
-            <nav class="navbar navbar-light flot-right" style="padding: .2rem .0rem; float: right;">
-                
+            <div class="col-6 col-md-3">
+              <nav class="navbar navbar-light flot-right" style="padding: .2rem .0rem; float: right;">
+                  
+                  <div class="mr-3 hide-on-mobile"><a href="#">Login</a> <a href="#">Daftar</a></div>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="mr-4" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="width: 20px; color: #343a40;" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-4" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" style="width: 20px; color: #343a40;" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search mr-0 mt-2 pencarian collapsed" style="width: 20px; color: #343a40;" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+              </nav>
+            </div>
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search mr-0 mt-2 pencarian collapsed" style="width: 20px; color: #343a40;" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-            </nav>
-          </div>
-
+        </div>
       </div>
-    </div>
   </div>
-</div>
   </div>
 
   <div class="content">
@@ -191,13 +193,16 @@
       window.onscroll = function() {myFunction()};
 
       var navbar = document.getElementById("navbar");
+      var logoikm = document.getElementById("logoIkm");
       var sticky = navbar.offsetTop;
 
       function myFunction() {
         if (window.pageYOffset >= sticky) {
           navbar.classList.add("sticky")
+          logoikm.classList.add("logo-50")
         } else {
           navbar.classList.remove("sticky");
+          logoikm.classList.remove("logo-50")
         }
       }
     </script>
